de92bf8205cb629c29e25357e591253c406cae57
ChaseDrum <chasedrum@mac.com>
2016-02-05 22:40:13 -0800

    added files to GitHub

--- old-skool-mac/ViewController.swift
+++ old-skool-mac/ViewController.swift
@@ -10,9 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mainLbl: UILabel!
+    
+    var phrases = ["Booting from floppy...\n", "Reading from disk\n", "Updating registry...\n", "..............\n", ".................\n", "...................................\n", "Welcome Jessica \n It is nice to see you again."]
+    // typing \n puts the writing onto the next line
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        mainLbl.text = ""
+//        for var x = 0; x < phrases.count; x++ {
+//            var txt = mainLbl.text!
+//            txt += phrases[x]
+//            mainLbl.text = txt
+//   It's perfectly fine to write it the way above another is listed below
+//        }
+      
+        
+//        var x = 0
+//        repeat {
+//            var txt = mainLbl.text!
+//            txt += phrases[x]
+//            mainLbl.text = txt
+//            x++
+//        } while x < phrases.count;
+//        
+
+    // another even more short hand way to do the same 2 previous operations is below
+        for phrase in phrases {
+            var txt = mainLbl.text!
+            txt += phrase
+            mainLbl.text = txt
+            
+            
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
